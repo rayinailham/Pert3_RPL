5b557cc35f01d022a83c6cc0c93f776ca47f1921
Rayi Na Ilham <rayinailham@gmail.com>
2025-11-25 16:49:55 +0700

    the programs...

---
+++ src/main/java/com/mycompany/pert4_4ia01_51422270/Pert4_4IA01_51422270.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pert3_4ia01_51422270;
+
+/**
+ *
+ * @author ASUS
+ */
+public class pert3_4IA01_51422270 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
